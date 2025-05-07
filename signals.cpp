eb84f74b55e25b58b539a5da85c574f66cec3703
NirMillerHP <nir.miller@campus.technion.ac.il>
2025-05-07 11:19:25 +0000

    [HW01] signals.cpp integration

--- signals.cpp
+++ signals.cpp
@@ -6,7 +6,20 @@
 using namespace std;
 
 void ctrlCHandler(int sig_num) {
-    // TODO: Add your implementation
+    
+    pid_t fg_pid = SmallShell::getInstance().get_fg_pid();
+
+    std::cout<<"smash: got ctrl-C"<<std::endl;
+    if(SmallShell::getInstance().get_pid() == fg_pid){
+        return;
+    }
+    
+    if(my_kill(fg_pid, SIGKILL) != 0){
+        perror("smash error: kill");
+    }
+
+    std::cout<<"smash: process "<< fg_pid << " was killed" << std::endl;
+    return;
 }
 
 int my_kill(pid_t pid, int sig) {
